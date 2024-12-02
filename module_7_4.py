84c73215a95d78ebdc03cef9d51bb2178d2fc8ff
IlonaWishec <ilonawishec.management@gmail.com>
2024-12-02 17:55:54 +0300

    module_7_4.py

--- module_7_4.py
+++ module_7_4.py
@@ -17,7 +17,7 @@ print(f'Команды решили {score_1} и {score_2} задач')
 if score_1 > score_2 or (score_1 == score_2 and team1_time > team2_time):
     challenge_result = 'Победа команды Мастера кода!'
 elif score_1 < score_2 or (score_1 == score_2 and team1_time < team2_time):
-    challenge_result = f'Победа команды Волшебники Данных!'
+    challenge_result = 'Победа команды Волшебники Данных!'
 else:
     challenge_result = 'Ничья!'
 print(f"Результат битвы: {challenge_result}")
